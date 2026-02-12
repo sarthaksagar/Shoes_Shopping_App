4a6b5a2e2806719f43ae94951a1fb0bc2c62ea0a
sagar <sagarsarthak111@gmail.com>
2026-02-12 21:27:51 +0530

    add one file

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:footware_page/controller/cart_provider.dart';
-import 'package:provider/provider.dart'; 
 import 'package:footware_page/controller/home_controller.dart';
 import 'package:footware_page/controller/login_conroller.dart';
 import 'package:footware_page/firebase_options.dart';
 import 'package:footware_page/pages/home_page.dart';
 import 'package:footware_page/pages/login_page.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: firebaseOptions);
-
 
   Get.put(LoginController());
   Get.put(HomeController());
 
   runApp(
     ChangeNotifierProvider(
-      create: (context) => CartProvider(), 
+      create: (context) => CartProvider(),
       child: const MyApp(),
     ),
   );
@@ -34,13 +32,11 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Footwear Store',
-      theme: ThemeData(primarySwatch: Colors.deepPurple,
-      useMaterial3: true,
-      ),
-      initialRoute: '/login',  
+      theme: ThemeData(primarySwatch: Colors.deepPurple, useMaterial3: true),
+      initialRoute: '/login',
       getPages: [
         GetPage(name: '/login', page: () => const LoginPage()),
-        GetPage(name: '/home', page: () => const HomePage()), 
+        GetPage(name: '/home', page: () => const HomePage()),
       ],
     );
   }
